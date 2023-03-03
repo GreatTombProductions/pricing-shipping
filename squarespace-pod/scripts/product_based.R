b36debc0e392942c5615efb5985ece9363623edd
Raymond Heberer <ray.heberer@gmail.com>
2023-03-03 15:03:17 -0800

    Update formulas

--- squarespace-pod/scripts/product_based.R
+++ squarespace-pod/scripts/product_based.R
@@ -1,14 +1,16 @@
 #' Price Items based on their Average Shipping Costs
 library(magrittr)
+library(ggplot2)
 
-margin <- 1.03
+min_margin <- 1.05
 
 data <- readRDS(here::here("squarespace-pod/data/production_costs.rds"))
 
 # step 1: add some proportion of max shipping cost into price
 with_retail_price <- data %>% 
+  #dplyr::filter(region != "Australia / New Zealand") %>% 
   dplyr::group_by(product) %>% 
-  dplyr::mutate(baked_in_shipping_cost = min(shipping_cost) + 0.2 * (max(shipping_cost) - min(shipping_cost))) %>% 
+  dplyr::mutate(baked_in_shipping_cost = min(shipping_cost) + 0.1 * (max(shipping_cost) - min(shipping_cost))) %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(
     retail_price = max_production_cost + baked_in_shipping_cost,
@@ -24,14 +26,29 @@ with_shipping_price <- with_retail_price %>%
     total_price = retail_price + shipping_price
   ) %>% 
   dplyr::mutate(
-    retail_price = dplyr::if_else((total_price / total_cost) >= margin, retail_price, retail_price * margin),
-    total_price = retail_price + shipping_price
+    retail_price = dplyr::if_else((total_price / total_cost) >= min_margin, retail_price, min_margin * total_cost - shipping_price)
   ) %>% 
-  dplyr::ungroup()
+  dplyr::ungroup() %>% 
+  dplyr::group_by(product) %>% 
+  dplyr::mutate(
+    retail_price = max(retail_price)
+  ) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(
+    total_price = retail_price + shipping_price,
+    margin = (total_price / total_cost)
+  )
 
 shipping_prices <- dplyr::distinct(with_shipping_price, region, shipping_price)
 product_prices <- dplyr::distinct(with_shipping_price, product, retail_price)
 
-View(shipping_prices)
-View(product_prices)
-View(with_shipping_price)
+View(shipping_prices, "Shipping Prices")
+View(product_prices, "Product Prices")
+View(dplyr::select(with_shipping_price, product, region, shipping_cost, max_production_cost, total_cost, retail_price, shipping_price, total_price, margin), "Margins")
+
+ggplot(with_shipping_price, aes(x = factor(region), y = margin)) +
+  geom_boxplot() +
+  ylim(1, 0.05 + max(with_shipping_price$margin))
+
+ggplot(product_prices, aes(x = retail_price, text = product)) +
+  geom_dotplot()
